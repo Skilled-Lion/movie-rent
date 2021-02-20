5448aba649fdc93ff08fa8c16fb36c8d85f9a150
Kaushal Kumar Singh <kaushal.singh@redbus.com>
2021-02-20 17:17:57 +0530

    intial app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace movie_rent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
